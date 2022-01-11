f994a6f48c34de65bd70849e0f90e1c386ffe750
Cwishart203 <93050589+Cwishart203@users.noreply.github.com>
2022-01-11 13:34:20 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,52 +1,79 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 // const questions = [description, installation, usage, license, contributing, tests, questions];
-inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "description",
-            message: "Please enter a description"
-        },
-        {
-            type: "input",
-            name: "installation",
-            message: "Please enter installation instructions"
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "Please enter usage instructions"
-        },
-        {
-            type: "list",
-            name: "license",
-            message: "Please choose a license"
-        },
-        {
-            type: "input",
-            name: "contributing",
-            message: "Please input contributing members"
-        },
-        {
-            type: "input",
-            name: "test",
-            message: "Please enter test information"
-        }
-    ])
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter a title for your project"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description for your project"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter any installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter any usage instructions"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please choose a license",
+        choices: ["MIT", "Apache", "Mozilla", "None"],
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please input any contributing members"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please enter test information"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your github username"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Successfully generated Readme")
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    promptUser()
-    .then((answers) => fs.writeFileSync('readme.md', generateHTML(answers)))
-    .then(() => console.log('Successfully wrote to readme.md'))
-    .catch((err) => console.error(err));
-}
+    inquirer.prompt(questions).then((answers) => {
+        console.log(answers);
+        var readMeAnswers = generateMarkdown(answers);
+        writeToFile("README.md", readMeAnswers);
+    });
+};
 
 // Function call to initialize app
 init();
