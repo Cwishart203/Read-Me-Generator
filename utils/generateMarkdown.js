72304aee6db4cce3910bc5eb3602b35b5f2f9878
Cwishart203 <93050589+Cwishart203@users.noreply.github.com>
2022-01-11 13:38:56 -0500

    License part of generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,42 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
+  if (license === "MIT") {
+    return "https://img.shields.io/static/v1?label=License&message=MIT&color=blue&?style=plastic&link=" + renderLicenseLink(license);
+  }
+  else if (license === "Mozilla") {
+    return "https://img.shields.io/static/v1?label=License&message=Mozilla&color=red&?style=plastic&link=" + renderLicenseLink(license);
+  }
+  else if (license === "Apache") {
+    return "https://img.shields.io/static/v1?label=License&message=Apache&color=green&?style=plastic&link=" + renderLicenseLink(license);
+  }
+  else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-
+  if (license === "MIT") {
+    return "https://choosealicense.com/licenses/mit/";
+  }
+  else if (license === "Mozilla") {
+    return "https://choosealicense.com/licenses/mpl-2.0/"
+  }
+  else if (license === "Apache") {
+    return "https://choosealicense.com/licenses/apache-2.0/";
+  }
+  else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
+  return `# License
+  ## Lincense used [${license}](${renderLicenseLink(license)})`
 }
 
 // TODO: Create a function to generate markdown for README
